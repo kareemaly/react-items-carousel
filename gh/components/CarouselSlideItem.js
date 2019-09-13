c9aed79dd49f833eea194df460ae6e6c2cac1a98
Kareem Mohamed <kareem3d.a@gmail.com>
2019-09-13 06:50:58 +0200

    improv: update demo to use local assets

--- gh/components/CarouselSlideItem.js
+++ gh/components/CarouselSlideItem.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import range from 'lodash/range';
+import nature from '../assets/images/nature.jpeg';
+import nature_0 from '../assets/images/nature_0.jpeg';
+import nature_1 from '../assets/images/nature_1.jpeg';
+import nature_2 from '../assets/images/nature_2.jpeg';
+import nature_3 from '../assets/images/nature_3.jpeg';
+import nature_4 from '../assets/images/nature_4.jpeg';
+import nature_5 from '../assets/images/nature_5.jpeg';
+
+const items = [
+  nature,
+  nature_0,
+  nature_1,
+  nature_2,
+  nature_3,
+  nature_4,
+  nature_5,
+];
 
 export const PlaceholderItem = styled.div`
   height: 200px;
@@ -16,5 +33,5 @@ export const SlideItem = styled.div`
 `;
 
 export const createImageChildren = n => range(n).map((i) => (
-  <SlideItem key={i} src={`https://placeimg.com/380/200/nature?i=${i}`} />
+  <SlideItem key={i} src={items[i%items.length]} />
 ));
